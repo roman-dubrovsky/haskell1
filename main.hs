b837be8999d22a502b1cad5b356305e2afca82c2
Roman Dubrovsky <roma2107946@gmail.com>
2015-12-09 21:57:27 +0300

    Fix custom csv

--- main.hs
+++ main.hs
@@ -2,16 +2,14 @@ module Main where
 
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector as V
--- from cassava
 import Data.Csv
 
--- a simple type alias for data
-type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+type Groups = ([Int])
 
 main :: IO ()
+
 main = do
-  csvData <- BL.readFile "batting.csv"
-  let v = decode NoHeader csvData :: Either String (V.Vector BaseballStats)
-  let summed = fmap (V.foldr summer 0) v
-  putStrLn $ "Total atBats was: " ++ (show summed)
-  where summer (name, year, team, atBats) n = n + atBats
+  csvData <- BL.readFile "butterfly.txt"
+  let csv = decode NoHeader csvData :: Either String (V.Vector Groups)
+
+  print "Hello"

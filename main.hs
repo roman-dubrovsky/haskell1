4fb17ba779fc8b9231cab6d49e3d452b46c301dc
Roman Dubrovsky <roma2107946@gmail.com>
2015-12-10 20:09:56 +0300

    Add ranges

--- main.hs
+++ main.hs
@@ -4,6 +4,14 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector as V
 import Data.Csv
 
+-- ===== ranges =====
+
+hammingRange :: Floating a => [a] -> [a] -> a
+hammingRange x y = foldl (+) 0 $ map abs $ zipWith (-) x y
+
+evclideRange :: Floating a => [a] -> [a] -> a
+evclideRange x y = sqrt $ foldl (+) 0 $ map (** 2) $ zipWith (-) x y
+
 type Groups = ([Int])
 
 main :: IO ()
